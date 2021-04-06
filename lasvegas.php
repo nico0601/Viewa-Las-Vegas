401608c13d3c4f7b75a2c3f0a873ced6e4d2f9eb
N. Naumann <nico.naumann@htl.rennweg.at>
2021-04-06 15:42:09 +0200

    First Commit

---
+++ lasvegas.php
@@ -0,0 +1,56 @@
+<?php
+
+
+class lasvegas
+{
+
+    //define variables
+
+    private string $name;
+    private int $pools;
+    private int $saunas;
+    private int $cocktailbars;
+    private string $source;
+
+    //Constructor
+
+    public function __construct(string $name, int $pools, int $saunas, int $cocktailbars, string $source)
+    {
+        $this->name = $name;
+        $this->pools = $pools;
+        $this->saunas = $saunas;
+        $this->cocktailbars = $cocktailbars;
+        $this->source = $source;
+    }
+
+    //Getters
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+
+    public function getPools(): int
+    {
+        return $this->pools;
+    }
+
+
+    public function getSaunas(): int
+    {
+        return $this->saunas;
+    }
+
+
+    public function getCocktailbars(): int
+    {
+        return $this->cocktailbars;
+    }
+
+
+    public function getSource(): string
+    {
+        return $this->source;
+    }
+}
